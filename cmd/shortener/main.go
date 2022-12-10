4f78b1190f127eaa5618f2a7d1eb17a8829a3597
blokhinnv <darenne@yandex.ru>
2022-12-10 20:02:19 +0300

    added air hot reloading

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -4,11 +4,17 @@ package main
 // docker run --name redis-test-instance -p 6379:6379 -d redis
 
 import (
+	"os"
+
 	s "github.com/blokhinnv/shorty/internal/app/server"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	godotenv.Load("local.env")
+	if len(os.Args) > 1 && os.Args[1] == "dev" {
+		godotenv.Load("dev.env")
+	} else {
+		godotenv.Load("local.env")
+	}
 	s.RunServer()
 }
