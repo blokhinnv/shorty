1eae80e4b6615ec9ee17b546406b0f009bc984c5
blokhinnv <blokhin.nv@bk.ru>
2022-12-20 14:32:13 +0300

    fixed middleware description (found during 1v1)

--- internal/app/server/routes/middleware/request_decompess.go
+++ internal/app/server/routes/middleware/request_decompess.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-// Middleware для сжатия ответа
+// Middleware для декодирования запроса
 func RequestGZipDecompress(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Header.Get("Content-Encoding") == "gzip" {
