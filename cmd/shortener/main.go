6695c0e23abf050f6c8f8e17eb865cdd22b1c863
blokhinnv <darenne@yandex.ru>
2023-03-05 22:44:03 +0300

    restored env in main

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -5,7 +5,9 @@ package main
 
 import (
 	"flag"
+	"os"
 
+	"github.com/joho/godotenv"
 	log "github.com/sirupsen/logrus"
 
 	s "github.com/blokhinnv/shorty/internal/app/server"
@@ -27,6 +29,20 @@ func parseConfig(cfg *config.FlagConfig) {
 }
 
 func main() {
+	// для разработки использую air, его приходится запускать их корня
+	// проекта, т.к. мне нужно мониторить изменения всех файлов проекта
+	// настроил air так, что собранный сервер запускается с аргументом dev,
+	// так что тут я могу понять, какие переменные брать
+	// если стартовать без air из папки /shortener, то все норм
+	if len(os.Args) > 1 && os.Args[1] == "dev" {
+		godotenv.Load("dev.env")
+	} else {
+		godotenv.Load("local.env")
+	}
+	// флаги надо собрать в одном месте на старте
+	// и прокидывать через кучу слоев....
+	// раньше это была глобальная переменная для пакета
+
 	flagCfg := config.FlagConfig{}
 	parseConfig(&flagCfg)
 	serverCfg, err := config.NewServerConfig(&flagCfg)
