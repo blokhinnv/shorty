dc94fbcf4a1ebc36f6e8157c9ddb4fedccfb3212
blokhinnv <darenne@yandex.ru>
2023-03-05 22:21:29 +0300

    upd

--- internal/app/database/postgres/create.go
+++ internal/app/database/postgres/create.go
@@ -5,13 +5,11 @@ import (
 	"fmt"
 
 	"github.com/jackc/pgx/v5/pgxpool"
-	log "github.com/sirupsen/logrus"
 )
 
 // SQL-запрос для создания таблицы для Url.
 const createSQL = `
-DROP TABLE IF EXISTS Url;
-CREATE TABLE Url(
+CREATE TABLE IF NOT EXISTS Url(
 	encoding_id SERIAL PRIMARY KEY,
 	url VARCHAR NOT NULL,
 	url_id VARCHAR NOT NULL,
@@ -20,31 +18,18 @@ CREATE TABLE Url(
 	requested_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	is_deleted BOOLEAN DEFAULT FALSE
 );
-CREATE UNIQUE INDEX idx_url ON Url(url);
-`
-
-// SQL-запрос для проверки наличия таблицы для Url.
-const existsSQL = `
-SELECT EXISTS (
-    SELECT FROM
-        pg_tables
-    WHERE
-        schemaname = 'public' AND
-        tablename  = 'Url'
-);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_url ON Url(url);
 `
 
 // InitDB инициализирует структуру БД для дальнейшей работы.
 func InitDB(conn *pgxpool.Pool, clearOnStart bool) error {
-	var exists bool
-	// при инициализации создадим БД, если ее не существует
-	if err := conn.QueryRow(context.Background(), existsSQL).Scan(&exists); err != nil {
+	if _, err := conn.Exec(context.Background(), createSQL); err != nil {
 		return fmt.Errorf("can't create table Url: %v", err)
 	}
-	if !exists || clearOnStart {
-		log.Infoln("INIT DB", exists, clearOnStart)
-		if _, err := conn.Exec(context.Background(), createSQL); err != nil {
-			return fmt.Errorf("can't create table Url: %v", err)
+
+	if clearOnStart {
+		if _, err := conn.Exec(context.Background(), clearSQL); err != nil {
+			return fmt.Errorf("can't clear table: %v", err)
 		}
 	}
 	return nil
